d7fc6b9ef47920f664b90183a41360c721d1118e
Phillip Lehner <plehner@bomgar.com>
2019-08-14 15:30:23 -0500

    Updated to Dotstar and purge of touch controls

--- NeoPixelFireflies/Firefly.ino
+++ NeoPixelFireflies/Firefly.ino
@@ -1,14 +1,15 @@
 Firefly::Firefly () {
   hueCenter = 0.85;
   hueRange = 0.2;
-  baseSpeed = 0.02;
+  baseSpeed = 0.01;
   _brightness = 0.0;
   _fadeSpeed = 0.0;
   _isGlowing = false;
 }
 
 void Firefly::ignite() {
-  _hue = hueCenter + (random(100)-50)*0.01*hueRange;
+//  _hue = hueCenter + (random(101)-50)*0.01*hueRange;
+  _hue = 0.22;
   _fadeSpeed = baseSpeed*(random(100)*0.01 + 0.5);
   _isGlowing = true;
 }
@@ -37,14 +38,12 @@ void Firefly::animate() {
 uint32_t Firefly::getColor() {
   uint8_t w = 255*_hue;
   if(w < 85) {
-    return Adafruit_NeoPixel::Color(w*3*_brightness, (255-w*3)*_brightness, 0);
+    return Adafruit_DotStar::Color(w*3*_brightness, (255-w*3)*_brightness, 0);
   } else if(w < 170) {
     w -= 85;
-    return Adafruit_NeoPixel::Color((255-w*3)*_brightness, 0, w*3*_brightness);
+    return Adafruit_DotStar::Color((255-w*3)*_brightness, 0, w*3*_brightness);
   } else {
     w -= 170;
-    return Adafruit_NeoPixel::Color(0, w*3*_brightness, (255-w*3)*_brightness);
+    return Adafruit_DotStar::Color(0, w*3*_brightness, (255-w*3)*_brightness);
   }  
 }
-
-
--- NeoPixelFireflies/NeoPixelFireflies.ino
+++ NeoPixelFireflies/NeoPixelFireflies.ino
@@ -1,13 +1,14 @@
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_LSM303_U.h>
-#include <Adafruit_NeoPixel.h>
+//#include <Wire.h>
+//#include <Adafruit_Sensor.h>
+//#include <Adafruit_LSM303_U.h>
+#include <SPI.h>
+#include <Adafruit_DotStar.h>
 
 #define PIN 6
-#define NUM_PIXELS 7
-#define MAX_GLOWING_PIXELS 4
-#define MOVE_THRESHOLD 1.0 // Quite sensitive
-#define STATIONARY_LATENCY 30000 // 30s glow after last move 
+#define NUM_PIXELS 1
+#define MAX_GLOWING_PIXELS 1
+//#define MOVE_THRESHOLD 1.0 // Quite sensitive
+//#define STATIONARY_LATENCY 30000 // 30s glow after last move 
 
 class Firefly {
   private:
@@ -29,53 +30,38 @@ class Firefly {
     void animate();    
 };
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
+#define DATAPIN    7
+#define CLOCKPIN   8
+Adafruit_DotStar strip(NUM_PIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+//Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
 
 Firefly ff[NUM_PIXELS];
-boolean moving;
-double lastVector;
-unsigned long lastSensorTime;
-unsigned long lastMovingTime;
+//boolean moving;
+//double lastVector;
+//unsigned long lastSensorTime;
+//unsigned long lastMovingTime;
 
 void setup() {
-  Serial.begin(9600);
-
-  randomSeed(analogRead(0));
-
-  if (!accel.begin()) {
-    Serial.println("Oops ... unable to initialize the LSM303. Check your wiring!");
-    while (1);
-  }
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
+    clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
+  #endif
 
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
- 
-  checkMoving();
-  delay(100); // Allow time for the first movement check
 }
 
 void loop() {
-  // Check for movement every 100ms
-  unsigned long time = millis();
-  if(abs(time - lastSensorTime) >= 100) {
-    checkMoving();
-    lastSensorTime = time;
-    if(moving) {
-      lastMovingTime = time;
-    }
-  }
-
+  
   int glowingPixelCount = 0;
   // do we have enough lit pixels?
-  for(int i = 0; i < NUM_PIXELS; i++) {
-    if(ff[i].isGlowing()) {
-      glowingPixelCount++;
-    }
-  }
+//  for(int i = 0; i < NUM_PIXELS; i++) {
+//    if(ff[i].isGlowing()) {
+//      glowingPixelCount++;
+//    }
+//  }
   
   for(int i = 0; i < NUM_PIXELS; i++) {
-    if(abs(time - lastMovingTime) <= STATIONARY_LATENCY && !ff[i].isGlowing() && glowingPixelCount < MAX_GLOWING_PIXELS && random(100) < 5) {
+    if(!ff[i].isGlowing() && glowingPixelCount < MAX_GLOWING_PIXELS && random(100) < 5) {
       ff[i].ignite();
       glowingPixelCount++;
     }
@@ -86,16 +72,12 @@ void loop() {
   delay(10);
 }
 
-void checkMoving() {
-  sensors_event_t event; 
-  accel.getEvent(&event);
-  double newVector = sqrt(event.acceleration.x*event.acceleration.x + event.acceleration.y*event.acceleration.y + event.acceleration.z*event.acceleration.z);
-
-  // are we moving 
-  moving = (abs(newVector - lastVector) > MOVE_THRESHOLD);
-  lastVector = newVector;
-}
-
-
-
-
+//void checkMoving() {
+//  sensors_event_t event; 
+//  accel.getEvent(&event);
+//  double newVector = sqrt(event.acceleration.x*event.acceleration.x + event.acceleration.y*event.acceleration.y + event.acceleration.z*event.acceleration.z);
+//
+//  // are we moving 
+//  moving = (abs(newVector - lastVector) > MOVE_THRESHOLD);
+//  lastVector = newVector;
+//}
